44c3f1dfc1cb2044c0f9e99c97b6e8797e53e41a
selimzade <wselimzade@mail.ru>
2023-05-24 13:09:24 +0400

    menu scrool test

--- js/app.js
+++ js/app.js
@@ -1,8 +1,10 @@
 "use strict";
 
+//! Change page and return.
+
 const weather = document.querySelector(".date-clock-weather-content");
 const defaultBackground = document.querySelector(".default");
-
+const returnArrow = document.querySelector("#arrow-left");
 const firstSection = document.querySelector(".first-section");
 const thirdSection = document.querySelector(".third-section");
 const secondPageFirstSection = document.querySelector(
@@ -12,29 +14,38 @@ const secondPageThirdSection = document.querySelector(
   ".second-page-third-section"
 );
 
-const returnArrow = document.querySelector("#arrow-left");
-
+//? Run Function when click weather area.
 const weatherInfo = () => {
-  
-    firstSection.style.display = "none";
-    thirdSection.style.display = "none";
-    secondPageFirstSection.style.display = "block";
-    secondPageThirdSection.style.display = "block";
+  firstSection.style.display = "none";
+  thirdSection.style.display = "none";
+  secondPageFirstSection.style.display = "block";
+  secondPageThirdSection.style.display = "block";
 
-    defaultBackground.classList.add("bg");
-  
+  defaultBackground.classList.add("bg");
 };
 
+//? Run Function when return default page with return arrow.
 const returnDefaultPage = () => {
-  
-    secondPageFirstSection.style.display = "none";
-    secondPageThirdSection.style.display = "none";
-    firstSection.style.display = "block";
-    thirdSection.style.display = "block";
+  secondPageFirstSection.style.display = "none";
+  secondPageThirdSection.style.display = "none";
+  firstSection.style.display = "block";
+  thirdSection.style.display = "block";
 
-    defaultBackground.classList.remove("bg");
-  
+  defaultBackground.classList.remove("bg");
 };
 
 weather.addEventListener("click", weatherInfo);
 returnArrow.addEventListener("click", returnDefaultPage);
+
+//! Slider weather menu for time
+
+const scrollbarThumb = document.querySelector('.second-section .dg-clock::-webkit-scrollbar-thumb');
+
+scrollbarThumb.addEventListener('mouseenter', function() {
+  scrollbarThumb.style.cursor = 'pointer';
+});
+
+scrollbarThumb.addEventListener('mouseleave', function() {
+  scrollbarThumb.style.cursor = 'auto';
+});
+
