aaa4938f6296c45d62d0f3ffaff7464b9023ec9f
selimzade <wselimzade@mail.ru>
2023-06-01 23:40:07 +0400

    add thrid section Weather Info Icon and Temp

--- js/app.js
+++ js/app.js
@@ -163,7 +163,7 @@ function onSucces(position) {
   const { latitude, longitude } = position.coords;
   API = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&units=metric&appid=${apiKey}`;
   hourlyAPI = `https://api.openweathermap.org/data/2.5/forecast?lat=${latitude}&lon=${longitude}&units=metric&cnt=10&appid=${apiKey}`;
-  dailyAPI = `https://api.openweathermap.org/data/2.5/forecast?dailylat=${latitude}&lon=${longitude}&cnt=40&units=metric&cnt=10&appid=${apiKey}`;
+  dailyAPI = `https://api.openweathermap.org/data/2.5/forecast?lat=${latitude}&lon=${longitude}&cnt=40&units=metric&cnt=10&appid=${apiKey}`;
 
   fetchData();
   alertError.style.display = "none";
@@ -185,7 +185,7 @@ function requestApi(city) {
 
   // dailyAPI = `https://api.openweathermap.org/data/2.5/forecast/daily?q=${city}&cnt=7&units=metric&appid=${apiKey}`;
   // daily API openweathermap documentation-nda bu cur yazilib. Ancaq işləmir. "daily" ifadəsini çıxardıqda hourly API qaytarır.
-  dailyAPI = `https://api.openweathermap.org/data/2.5/forecast/daily?q=${city}&cnt=40&units=metric&appid=${apiKey}`; // işləmir
+  dailyAPI = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&cnt=40&units=metric&appid=${apiKey}`; // işləmir
 
   fetchData();
 }
@@ -215,6 +215,427 @@ function dailyDetails(dInfo) {
   if (dInfo.cod == "404") {
     console.log("daily data is don't found");
   } else {
+    const dailyData = dInfo.list;
+
+    // daily data number convert month name
+    switch (month) {
+      case "January":
+        month = "01";
+        break;
+      case "February":
+        month = "02";
+        break;
+      case "March":
+        month = "03";
+        break;
+      case "April":
+        month = "04";
+        break;
+      case "May":
+        month = "05";
+        break;
+      case "June":
+        month = "06";
+        break;
+      case "July":
+        month = "07";
+        break;
+      case "August":
+        month = "08";
+        break;
+      case "September":
+        month = "09";
+        break;
+      case "October":
+        month = "10";
+        break;
+      case "November":
+        month = "11";
+        break;
+      case "December":
+        month = "12";
+        break;
+    }
+
+    // tomorrow and next day
+    let day2 = String(Number(day) + 1);
+    let day3 = String(Number(day) + 2);
+    let day4 = String(Number(day) + 3);
+    let day5 = String(Number(day) + 4);
+
+    // add "0" after the day number
+    day = day < 10 ? "0" + day : day;
+    day2 = day2 < 10 ? "0" + day2 : day2;
+    day3 = day3 < 10 ? "0" + day3 : day3;
+    day4 = day4 < 10 ? "0" + day4 : day4;
+    day5 = day5 < 10 ? "0" + day5 : day5;
+
+    // tomorrow and next day data time
+    const tomorrowData = `${year}-${month}-${day2}`;
+    const theDayAfterTomorrowData = `${year}-${month}-${day3}`;
+    const inTwoDaysData = `${year}-${month}-${day4}`;
+    const inThreeDaysData = `${year}-${month}-${day5}`;
+
+    // tomorrow and next day temp of while day
+    let tomorrowArr = [];
+    let theDayAfterTomorrowArr = [];
+    let inTwoDaysArr = [];
+    let inThreeDaysArr = [];
+
+    // tomorrow and next day ID for 12:00
+    let tomorrowAverageTimeID;
+    let theDayAfterTomorrowAverageTimeID;
+    let inTwoDaysAverageTimeID;
+    let inThreeDaysAverageTimeID;
+
+    // array list all weather icon in thirdsection
+    let weatherInfoIcon = thirdSection.querySelectorAll("#weather-info-icon");
+
+    // weather icon for tomorrow and next day
+    let tomorrowWeatherIcon = weatherInfoIcon[0];
+    let theDayAfterTomorrowWeatherIcon = weatherInfoIcon[1];
+    let inTwoDaysWeatherIcon = weatherInfoIcon[2];
+    let inThreeDaysWeatherIcon = weatherInfoIcon[3];
+
+    // array list weather info for tomorrow and next day
+    const weekDayInfoAll = thirdSection.querySelectorAll(".weekDay-info");
+
+    // weather info for tomorrow and next day
+    let tomorrowWeekDayInfo;
+    let theAfterDayTomorrowWeekDayInfo;
+    let inTwoDaysWeekDayInfo;
+    let inThreeDaysWeekDayInfo;
+
+    // tomorrow and next day temp of while day add to array
+    for (let i = 0; i <= 39; i++) {
+      let dailyDataAll = dailyData[i].dt_txt.split(" ")[0];
+      let dailyDataTime = dailyData[i].dt_txt.split(" ")[1];
+
+      if (dailyDataAll == tomorrowData) {
+        tomorrowArr.push(dailyData[i].main.temp);
+        if (dailyDataTime == "12:00:00") {
+          tomorrowAverageTimeID = dailyData[i].weather[0].id;
+          tomorrowWeekDayInfo = dailyData[i].weather[0].description;
+        }
+      } else if (dailyDataAll == theDayAfterTomorrowData) {
+        theDayAfterTomorrowArr.push(dailyData[i].main.temp);
+        if (dailyDataTime == "12:00:00") {
+          theDayAfterTomorrowAverageTimeID = dailyData[i].weather[0].id;
+          theAfterDayTomorrowWeekDayInfo = dailyData[i].weather[0].description;
+        }
+      } else if (dailyDataAll == inTwoDaysData) {
+        inTwoDaysArr.push(dailyData[i].main.temp);
+        if (dailyDataTime == "12:00:00") {
+          inTwoDaysAverageTimeID = dailyData[i].weather[0].id;
+          inTwoDaysWeekDayInfo = dailyData[i].weather[0].description;
+        }
+      } else if (dailyDataAll == inThreeDaysData) {
+        inThreeDaysArr.push(dailyData[i].main.temp);
+        if (dailyDataTime == "12:00:00") {
+          inThreeDaysAverageTimeID = dailyData[i].weather[0].id;
+          inThreeDaysWeekDayInfo = dailyData[i].weather[0].description;
+        }
+      }
+    }
+
+    console.log(
+      tomorrowAverageTimeID,
+      theDayAfterTomorrowAverageTimeID,
+      inTwoDaysAverageTimeID,
+      inThreeDaysAverageTimeID
+    );
+
+    //? Tomorrow weather info icon change for weather data
+    // clear
+    if (tomorrowAverageTimeID === 800) {
+      let clear = "01d";
+      tomorrowWeatherIcon.src = `https://openweathermap.org/img/w/${clear}.png`;
+      weekDayInfoAll[0].innerText = tomorrowWeekDayInfo;
+    }
+    //thunderstorm
+    else if (tomorrowAverageTimeID >= 200 && tomorrowAverageTimeID <= 232) {
+      let thunderstorm = "11d";
+      tomorrowWeatherIcon.src = `https://openweathermap.org/img/w/${thunderstorm}.png`;
+      weekDayInfoAll[0].innerText = tomorrowWeekDayInfo;
+    }
+    // drizzle
+    else if (tomorrowAverageTimeID >= 300 && tomorrowAverageTimeID <= 321) {
+      let drizzle = "09d";
+      tomorrowWeatherIcon.src = `https://openweathermap.org/img/w/${drizzle}.png`;
+      weekDayInfoAll[0].innerText = tomorrowWeekDayInfo;
+    }
+    // rain
+    else if (tomorrowAverageTimeID >= 500 && tomorrowAverageTimeID <= 531) {
+      let rain = "";
+      if (tomorrowAverageTimeID >= 500 && tomorrowAverageTimeID < 504) {
+        rain = "10d";
+      } else if (tomorrowAverageTimeID === 511) {
+        rain = "13d";
+      } else {
+        rain = "09d";
+      }
+      tomorrowWeatherIcon.src = `https://openweathermap.org/img/w/${rain}.png`;
+      weekDayInfoAll[0].innerText = tomorrowWeekDayInfo;
+    }
+    // snow
+    else if (tomorrowAverageTimeID >= 600 && tomorrowAverageTimeID <= 622) {
+      let snow = "13d";
+      tomorrowWeatherIcon.src = `https://openweathermap.org/img/w/${snow}.png`;
+      weekDayInfoAll[0].innerText = tomorrowWeekDayInfo;
+    }
+    // atmosphere
+    else if (tomorrowAverageTimeID >= 701 && tomorrowAverageTimeID <= 781) {
+      let atmosphere = "50d";
+      tomorrowWeatherIcon.src = `https://openweathermap.org/img/w/${atmosphere}.png`;
+      weekDayInfoAll[0].innerText = tomorrowWeekDayInfo;
+    }
+    // clouds
+    else if (tomorrowAverageTimeID >= 801 && tomorrowAverageTimeID <= 804) {
+      let clouds = "";
+      if (tomorrowAverageTimeID === 801) {
+        clouds = "02d";
+      } else if (tomorrowAverageTimeID === 802) {
+        clouds = "03d";
+      } else {
+        clouds = "04d";
+      }
+      tomorrowWeatherIcon.src = `https://openweathermap.org/img/w/${clouds}.png`;
+      weekDayInfoAll[0].innerText = tomorrowWeekDayInfo;
+    }
+
+    //? theDayAfterTomorrow weather info icon change for weather data
+    // clear
+    if (theDayAfterTomorrowAverageTimeID === 800) {
+      let clear = "01d";
+      theDayAfterTomorrowWeatherIcon.src = `https://openweathermap.org/img/w/${clear}.png`;
+      weekDayInfoAll[1].innerText = theAfterDayTomorrowWeekDayInfo;
+    }
+    //thunderstorm
+    else if (
+      theDayAfterTomorrowAverageTimeID >= 200 &&
+      theDayAfterTomorrowAverageTimeID <= 232
+    ) {
+      let thunderstorm = "11d";
+      theDayAfterTomorrowWeatherIcon.src = `https://openweathermap.org/img/w/${thunderstorm}.png`;
+      weekDayInfoAll[1].innerText = theAfterDayTomorrowWeekDayInfo;
+    }
+    // drizzle
+    else if (
+      theDayAfterTomorrowAverageTimeID >= 300 &&
+      theDayAfterTomorrowAverageTimeID <= 321
+    ) {
+      let drizzle = "09d";
+      theDayAfterTomorrowWeatherIcon.src = `https://openweathermap.org/img/w/${drizzle}.png`;
+      weekDayInfoAll[1].innerText = theAfterDayTomorrowWeekDayInfo;
+    }
+    // rain
+    else if (
+      theDayAfterTomorrowAverageTimeID >= 500 &&
+      theDayAfterTomorrowAverageTimeID <= 531
+    ) {
+      let rain = "";
+      if (
+        theDayAfterTomorrowAverageTimeID >= 500 &&
+        theDayAfterTomorrowAverageTimeID < 504
+      ) {
+        rain = "10d";
+      } else if (theDayAfterTomorrowAverageTimeID === 511) {
+        rain = "13d";
+      } else {
+        rain = "09d";
+      }
+      theDayAfterTomorrowWeatherIcon.src = `https://openweathermap.org/img/w/${rain}.png`;
+      weekDayInfoAll[1].innerText = theAfterDayTomorrowWeekDayInfo;
+    }
+    // snow
+    else if (
+      theDayAfterTomorrowAverageTimeID >= 600 &&
+      theDayAfterTomorrowAverageTimeID <= 622
+    ) {
+      let snow = "13d";
+      theDayAfterTomorrowWeatherIcon.src = `https://openweathermap.org/img/w/${snow}.png`;
+      weekDayInfoAll[1].innerText = theAfterDayTomorrowWeekDayInfo;
+    }
+    // atmosphere
+    else if (
+      theDayAfterTomorrowAverageTimeID >= 701 &&
+      theDayAfterTomorrowAverageTimeID <= 781
+    ) {
+      let atmosphere = "50d";
+      theDayAfterTomorrowWeatherIcon.src = `https://openweathermap.org/img/w/${atmosphere}.png`;
+      weekDayInfoAll[1].innerText = theAfterDayTomorrowWeekDayInfo;
+    }
+    // clouds
+    else if (
+      theDayAfterTomorrowAverageTimeID >= 801 &&
+      theDayAfterTomorrowAverageTimeID <= 804
+    ) {
+      let clouds = "";
+      if (theDayAfterTomorrowAverageTimeID === 801) {
+        clouds = "02d";
+      } else if (theDayAfterTomorrowAverageTimeID === 802) {
+        clouds = "03d";
+      } else {
+        clouds = "04d";
+      }
+      theDayAfterTomorrowWeatherIcon.src = `https://openweathermap.org/img/w/${clouds}.png`;
+      weekDayInfoAll[1].innerText = theAfterDayTomorrowWeekDayInfo;
+    }
+
+    //? in two days weather info icon change for weather data
+    // clear
+    if (inTwoDaysAverageTimeID === 800) {
+      let clear = "01d";
+      inTwoDaysWeatherIcon.src = `https://openweathermap.org/img/w/${clear}.png`;
+      weekDayInfoAll[2].innerText = inTwoDaysWeekDayInfo;
+    }
+    //thunderstorm
+    else if (inTwoDaysAverageTimeID >= 200 && inTwoDaysAverageTimeID <= 232) {
+      let thunderstorm = "11d";
+      inTwoDaysWeatherIcon.src = `https://openweathermap.org/img/w/${thunderstorm}.png`;
+      weekDayInfoAll[2].innerText = inTwoDaysWeekDayInfo;
+    }
+    // drizzle
+    else if (inTwoDaysAverageTimeID >= 300 && inTwoDaysAverageTimeID <= 321) {
+      let drizzle = "09d";
+      inTwoDaysWeatherIcon.src = `https://openweathermap.org/img/w/${drizzle}.png`;
+      weekDayInfoAll[2].innerText = inTwoDaysWeekDayInfo;
+    }
+    // rain
+    else if (inTwoDaysAverageTimeID >= 500 && inTwoDaysAverageTimeID <= 531) {
+      let rain = "";
+      if (inTwoDaysAverageTimeID >= 500 && inTwoDaysAverageTimeID < 504) {
+        rain = "10d";
+      } else if (inTwoDaysAverageTimeID === 511) {
+        rain = "13d";
+      } else {
+        rain = "09d";
+      }
+      inTwoDaysWeatherIcon.src = `https://openweathermap.org/img/w/${rain}.png`;
+      weekDayInfoAll[2].innerText = inTwoDaysWeekDayInfo;
+    }
+    // snow
+    else if (inTwoDaysAverageTimeID >= 600 && inTwoDaysAverageTimeID <= 622) {
+      let snow = "13d";
+      inTwoDaysWeatherIcon.src = `https://openweathermap.org/img/w/${snow}.png`;
+      weekDayInfoAll[2].innerText = inTwoDaysWeekDayInfo;
+    }
+    // atmosphere
+    else if (inTwoDaysAverageTimeID >= 701 && inTwoDaysAverageTimeID <= 781) {
+      let atmosphere = "50d";
+      inTwoDaysWeatherIcon.src = `https://openweathermap.org/img/w/${atmosphere}.png`;
+      weekDayInfoAll[2].innerText = inTwoDaysWeekDayInfo;
+    }
+    // clouds
+    else if (inTwoDaysAverageTimeID >= 801 && inTwoDaysAverageTimeID <= 804) {
+      let clouds = "";
+      if (inTwoDaysAverageTimeID === 801) {
+        clouds = "02d";
+      } else if (inTwoDaysAverageTimeID === 802) {
+        clouds = "03d";
+      } else {
+        clouds = "04d";
+      }
+      inTwoDaysWeatherIcon.src = `https://openweathermap.org/img/w/${clouds}.png`;
+      weekDayInfoAll[2].innerText = inTwoDaysWeekDayInfo;
+    }
+
+    //? in three days weather info icon change for weather data
+    // clear
+    if (inThreeDaysAverageTimeID === 800) {
+      let clear = "01d";
+      inThreeDaysWeatherIcon.src = `https://openweathermap.org/img/w/${clear}.png`;
+      weekDayInfoAll[3].innerText = inThreeDaysWeekDayInfo;
+    }
+    //thunderstorm
+    else if (
+      inThreeDaysAverageTimeID >= 200 &&
+      inThreeDaysAverageTimeID <= 232
+    ) {
+      let thunderstorm = "11d";
+      inThreeDaysWeatherIcon.src = `https://openweathermap.org/img/w/${thunderstorm}.png`;
+      weekDayInfoAll[3].innerText = inThreeDaysWeekDayInfo;
+    }
+    // drizzle
+    else if (
+      inThreeDaysAverageTimeID >= 300 &&
+      inThreeDaysAverageTimeID <= 321
+    ) {
+      let drizzle = "09d";
+      inThreeDaysWeatherIcon.src = `https://openweathermap.org/img/w/${drizzle}.png`;
+      weekDayInfoAll[3].innerText = inThreeDaysWeekDayInfo;
+    }
+    // rain
+    else if (
+      inThreeDaysAverageTimeID >= 500 &&
+      inThreeDaysAverageTimeID <= 531
+    ) {
+      let rain = "";
+      if (inThreeDaysAverageTimeID >= 500 && inThreeDaysAverageTimeID < 504) {
+        rain = "10d";
+      } else if (inThreeDaysAverageTimeID === 511) {
+        rain = "13d";
+      } else {
+        rain = "09d";
+      }
+      inThreeDaysWeatherIcon.src = `https://openweathermap.org/img/w/${rain}.png`;
+      weekDayInfoAll[3].innerText = inThreeDaysWeekDayInfo;
+    }
+    // snow
+    else if (
+      inThreeDaysAverageTimeID >= 600 &&
+      inThreeDaysAverageTimeID <= 622
+    ) {
+      let snow = "13d";
+      inThreeDaysWeatherIcon.src = `https://openweathermap.org/img/w/${snow}.png`;
+      weekDayInfoAll[3].innerText = inThreeDaysWeekDayInfo;
+    }
+    // atmosphere
+    else if (
+      inThreeDaysAverageTimeID >= 701 &&
+      inThreeDaysAverageTimeID <= 781
+    ) {
+      let atmosphere = "50d";
+      inThreeDaysWeatherIcon.src = `https://openweathermap.org/img/w/${atmosphere}.png`;
+      weekDayInfoAll[3].innerText = inThreeDaysWeekDayInfo;
+    }
+    // clouds
+    else if (
+      inThreeDaysAverageTimeID >= 801 &&
+      inThreeDaysAverageTimeID <= 804
+    ) {
+      let clouds = "";
+      if (inThreeDaysAverageTimeID === 801) {
+        clouds = "02d";
+      } else if (inThreeDaysAverageTimeID === 802) {
+        clouds = "03d";
+      } else {
+        clouds = "04d";
+      }
+      inThreeDaysWeatherIcon.src = `https://openweathermap.org/img/w/${clouds}.png`;
+      weekDayInfoAll[3].innerText = inThreeDaysWeekDayInfo;
+    }
+
+    // calculating temp avarage of day
+    function tempAvarage(one, two, three, four, five, six, seven, eight) {
+      return (one + two + three + four + five + six + seven + eight) / 8;
+    }
+
+    const weekDayInfo = thirdSection.querySelectorAll(".weekDay-dg-info");
+
+    weekDayInfo[0].innerText = `${Math.floor(
+      tempAvarage(...tomorrowArr)
+    )}º C`;
+    weekDayInfo[1].innerText = `${Math.floor(
+      tempAvarage(...theDayAfterTomorrowArr)
+    )}º C`;
+    weekDayInfo[2].innerText = `${Math.floor(
+      tempAvarage(...inTwoDaysArr)
+    )}º C`;
+    weekDayInfo[3].innerText = `${Math.floor(
+      tempAvarage(...inThreeDaysArr)
+    )}º C`;
+
     console.log(dInfo);
   }
 }
@@ -246,16 +667,23 @@ function hourlyDetails(hInfo) {
         .split(" ")[1]
         .slice(0, 5)} ${hpart}`;
 
+      // clear
       if (hId === 800) {
         let clear = "01d";
         hWIconArr.src = `https://openweathermap.org/img/w/${clear}.png`;
-      } else if (hId >= 200 && hId <= 232) {
+      }
+      //thunderstorm
+      else if (hId >= 200 && hId <= 232) {
         let thunderstorm = "11d";
         hWIconArr.src = `https://openweathermap.org/img/w/${thunderstorm}.png`;
-      } else if (hId >= 300 && hId <= 321) {
+      }
+      // drizzle
+      else if (hId >= 300 && hId <= 321) {
         let drizzle = "09d";
         hWIconArr.src = `https://openweathermap.org/img/w/${drizzle}.png`;
-      } else if (hId >= 500 && hId <= 504) {
+      }
+      // rain
+      else if (hId >= 500 && hId <= 531) {
         let rain = "";
         if (hId >= 500 && hId < 504) {
           rain = "10d";
@@ -265,13 +693,19 @@ function hourlyDetails(hInfo) {
           rain = "09d";
         }
         hWIconArr.src = `https://openweathermap.org/img/w/${rain}.png`;
-      } else if (hId >= 600 && hId <= 622) {
+      }
+      // snow
+      else if (hId >= 600 && hId <= 622) {
         let snow = "13d";
         hWIconArr.src = `https://openweathermap.org/img/w/${snow}.png`;
-      } else if (hId >= 701 && hId <= 781) {
+      }
+      // atmosphere
+      else if (hId >= 701 && hId <= 781) {
         let atmosphere = "50d";
         hWIconArr.src = `https://openweathermap.org/img/w/${atmosphere}.png`;
-      } else if (hId >= 801 && hId <= 804) {
+      }
+      // clouds
+      else if (hId >= 801 && hId <= 804) {
         let clouds = "";
         if (hId === 801) {
           clouds = "02d";
@@ -330,6 +764,7 @@ function weatherDetails(info) {
       fog === undefined ? "-" : `${fog}%`;
 
     //? weather icons for different temprature
+    // clear
     if (id === 800) {
       let clear = "01d";
       wIcon.src = `https://openweathermap.org/img/w/${clear}.png`;
@@ -512,7 +947,7 @@ const weekDaysNames = [
 let weekDayValue;
 if (weekDay >= 0 && weekDay <= 6) {
   weekDayValue = weekDaysNames[weekDay];
-  for (let i = 0; i <= 5; i++) {
+  for (let i = 0; i <= 3; i++) {
     let j = i + 1;
     WeekDayName[i].innerText = weekDaysNames[weekDay + j];
   }
